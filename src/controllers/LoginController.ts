0ff4fa8b70a1d0944bb9b3fe19d95ec106440c71
Joshua030 <Joseluy123>
2023-07-20 23:56:30 +0200

    Feat, add a decorator to handle middlewares

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -1,12 +1,17 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { controller } from "./decorators/controller";
 import { get } from "./decorators/routes";
+import { use } from "./decorators/use";
 
-
+function logger(req: Request, res: Response, next : NextFunction){
+  console.log('Request was made!!!');
+  next(); 
+}
 
 @controller('/auth')
 class LoginController {
   @get('/login')
+  @use(logger)
   getLogin(req: Request, res: Response): void {
     res.send(`
   <form method="POST">
